9d72d2f44ed06ca4f70c20e1ccdb2252e9816551
Александр <aleksandr@MacBook-Air-Aleksandr.local>
2023-10-13 23:24:00 +0300

    feat: add taiga

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,53 @@
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import {
+  TuiRootModule,
+  TuiDialogModule,
+  TuiAlertModule,
+  TUI_SANITIZER,
+} from '@taiga-ui/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TuiButtonModule, TuiDataListModule } from '@taiga-ui/core';
+import {
+  TuiIslandModule,
+  TuiTagModule,
+  TuiInputModule,
+  TuiDataListWrapperModule,
+  TuiMultiSelectModule,
+  TuiCheckboxLabeledModule,
+} from '@taiga-ui/kit';
+import { TuiTableModule, TuiTableFiltersModule } from '@taiga-ui/addon-table';
 import { MapModule } from './map/map.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MapModule],
-  providers: [],
+  imports: [
+    MapModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    TuiIslandModule,
+    TuiButtonModule,
+    TuiTableModule,
+    TuiTagModule,
+    TuiTableFiltersModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TuiInputModule,
+    TuiDataListModule,
+    TuiDataListWrapperModule,
+    TuiMultiSelectModule,
+    TuiCheckboxLabeledModule,
+  ],
+  exports: [],
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
